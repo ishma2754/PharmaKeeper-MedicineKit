c802f12e7b369de2db2d1a2d5b6ae4bf71b8b347
ISHMA KHAN <149327188+ishma2754@users.noreply.github.com>
2024-04-27 14:14:56 +0530

    Add files via upload

--- src/action.js
+++ src/action.js
@@ -96,7 +96,7 @@ export function applyFilter() {
       return; 
     }
 
-    
+    /*
     if (imageFile){
       const url = URL.createObjectURL(imageFile);
 
@@ -117,9 +117,10 @@ export function applyFilter() {
 
       URL.revokeObjectURL(url);
     }
+    */
     
 
-    /*if (imageFile) {
+    if (imageFile) {
       const reader = new FileReader();
       reader.onload = function(e) {
         const imageUrl = e.target.result;
@@ -136,7 +137,7 @@ export function applyFilter() {
       renderMedicinesList();
      
     }
-    */
+    
 
   }
 
